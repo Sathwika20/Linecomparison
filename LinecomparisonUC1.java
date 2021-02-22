f2ec15070fb1cd4437bf0f84c0680ee43492cf1d
G.Sathwika <gopirajsathwika1999@gmail.com>
2021-02-22 15:27:46 +0530

    added calculated length of line

---
+++ LinecomparisonUC1.java
@@ -0,0 +1,14 @@
+public class LinecomparisonUC1 {
+
+   public static void main(String[] args) {
+
+  double x1= Math.floor(Math.random() * 10);
+  double x2= Math.floor(Math.random() * 10);
+  double y1= Math.floor(Math.random() * 10);
+  double y2= Math.floor(Math.random() * 10);
+  double distance;
+
+   distance = Math.floor(Math.sqrt((x2-y1)*(x2-y1)) + ((y2-y1)*(y>
+   System.out.println("distance =" +distance);
+  }
+}
